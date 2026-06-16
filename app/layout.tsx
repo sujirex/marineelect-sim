7725cbb7f1c5d4fc08c159fe5ed254c9db356a97
Suji Kumar <suji@rexmious.com>
2026-06-16 14:49:13 +0530

    feat: switch font to Roboto

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,10 +9,4 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body>
-        <ThemeProvider>{children}</ThemeProvider>
-      </body>
-    </html>
-  );
-}
+    <html lan
