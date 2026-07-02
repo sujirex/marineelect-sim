ea93230ad317fc5cf3aac9a6e04fa9b8a9f35927
Suji Kumar <suji@rexmious.com>
2026-07-02 17:12:16 +0530

    Fix truncated layout.tsx; load Roboto; add type scale

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,4 +9,18 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lan
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  );
+}
